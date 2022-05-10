fe9644348809519dc2498482cb72432329a0b9a1
Ryan Appel <appel@fvtc.edu>
2022-05-10 12:09:39 -0500

    Fixed Allegro include errors. Added gameplay screen ref to level.

--- SpaceFighter/Level.h
+++ SpaceFighter/Level.h
@@ -7,6 +7,7 @@
 
 using namespace KatanaEngine;
 
+class GameplayScreen;
 
 class Level
 {
@@ -16,22 +17,24 @@ public:
 	Level();
 	virtual ~Level();
 
-	virtual void LoadContent(ResourceManager *pResourceManager);
+	virtual void LoadContent(ResourceManager* pResourceManager);
 
 	virtual void UnloadContent() = 0;
 
-	virtual void HandleInput(const InputState *pInput);
+	virtual void HandleInput(const InputState* pInput);
 
-	virtual void Update(const GameTime *pGameTime);
+	virtual void Update(const GameTime* pGameTime);
 
-	virtual void Draw(SpriteBatch *pSpriteBatch);
+	virtual void Draw(SpriteBatch* pSpriteBatch);
 
-	virtual void AddGameObject(GameObject *pGameObject) { m_gameObjects.push_back(pGameObject); }
+	virtual void AddGameObject(GameObject* pGameObject) { m_gameObjects.push_back(pGameObject); }
 
-	virtual void UpdateSectorPosition(GameObject *pGameObject);
+	virtual void UpdateSectorPosition(GameObject* pGameObject);
+
+	virtual void SetGameplayScreen(GameplayScreen* pGameplayScreen) { m_pGameplayScreen = pGameplayScreen; }
 
 	template <typename T>
-	T *GetClosestObject(const Vector2 position, const float range)
+	T* GetClosestObject(const Vector2 position, const float range)
 	{
 		float squaredRange = range * range;
 		if (range <= 0)
@@ -42,18 +45,18 @@ public:
 		}
 		float squaredDistance;
 
-		std::vector<GameObject *>::iterator m_it = m_gameObjects.begin();
-		T *pClosest = nullptr;
+		std::vector<GameObject*>::iterator m_it = m_gameObjects.begin();
+		T* pClosest = nullptr;
 
 		for (; m_it != m_gameObjects.end(); m_it++)
 		{
-			GameObject *pGameObject = *m_it;
+			GameObject* pGameObject = *m_it;
 			if (pGameObject->IsActive()) continue;
 
 			squaredDistance = (position - pGameObject->GetPosition()).LengthSquared();
 			if (squaredDistance < squaredRange)
 			{
-				T *pObject = dynamic_cast<T *>(pGameObject);
+				T* pObject = dynamic_cast<T*>(pGameObject);
 				if (pObject)
 				{
 					pClosest = pObject;
@@ -68,28 +71,30 @@ public:
 
 protected:
 
-	virtual CollisionManager *GetCollisionManager() { return m_pCollisionManager; }
+	virtual CollisionManager* GetCollisionManager() { return m_pCollisionManager; }
 
 
 private:
 
-	CollisionManager *m_pCollisionManager = nullptr;
+	CollisionManager* m_pCollisionManager = nullptr;
 
-	std::vector<GameObject *> *m_pSectors;
+	GameplayScreen* m_pGameplayScreen = nullptr;
+
+	std::vector<GameObject*>* m_pSectors;
 
 	Vector2 m_sectorCount;
 	Vector2 m_sectorSize;
 
 	unsigned int m_totalSectorCount;
 
-	std::vector<GameObject *> m_gameObjects;
-	std::vector<GameObject *>::iterator m_gameObjectIt;
+	std::vector<GameObject*> m_gameObjects;
+	std::vector<GameObject*>::iterator m_gameObjectIt;
 
-	PlayerShip *m_pPlayerShip;
-	std::vector<Projectile *> m_projectiles;
+	PlayerShip* m_pPlayerShip;
+	std::vector<Projectile*> m_projectiles;
 
 
-	void CheckCollisions(std::vector<GameObject *> &sector);
+	void CheckCollisions(std::vector<GameObject*>& sector);
 
 	virtual Vector2 GetSectorCount() const { return m_sectorCount; }
 
@@ -97,6 +102,6 @@ private:
 
 	virtual unsigned int GetTotalSectorCount() const { return m_totalSectorCount; }
 
-	virtual std::vector<GameObject *> *GetSectors() { return m_pSectors; }
+	virtual std::vector<GameObject*>* GetSectors() { return m_pSectors; }
 
 };
